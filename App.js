07391a921cbde5f6614cc0d7e9c4bae8ca741c96
9347630879 <145848207+9347630879@users.noreply.github.com>
2024-01-20 13:43:15 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 
-
+import { Component } from "react";
 import Welcome from "./Components/Welcome";
+class App extends Component{
+    state = {isLoggedIn:true}
+    render() {
+        const {isLoggedIn} = this.state
+       
+        return(
+            <div>
+                <Welcome greetings="Hello" name="Sai"/>
+                {isLoggedIn? <button>Logout</button> : <button>Login</button>}
+            
+            </div>
+        )
+    }
+}
 
 
-const App = () => <Welcome/>
+
 export default App;
